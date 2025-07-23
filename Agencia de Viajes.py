c0dd3bf04578db4aaed596896103336bb7476e19
Marvin291-dev <marvin.velasquez@udeo.edu.gt>
2025-07-23 10:14:38 -0600

    primer commit

---
+++ Agencia de Viajes.py
@@ -0,0 +1,10 @@
+Viajes = {}
+
+while True:
+    print("Menu de opciones")
+    print("1. Ingreso de Clientes")
+    print("2. El listado de clientes con sus destinos")
+    print("3 El total de destinos registrados")
+    print("4 El cliente con más destinos")
+    try:
+        opcion = int(input("Selecciona una opcion: "))
